ce13f4738da3b5479132d8a85406938bfaacb3a2
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-05-13 15:50:21 +0200

    Deterministic Contract Deployments

--- script/DeployBatchCaller.s.sol
+++ script/DeployBatchCaller.s.sol
@@ -6,21 +6,29 @@ import "forge-std/Vm.sol";
 import "src/BatchCallAndSponsor.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Create2.sol";
 
 contract DeployBatchCaller is Script {
     BatchCallAndSponsor public batchCaller;
+    // This salt will be used for CREATE2 deployment
+    bytes32 public constant SALT = keccak256("BatchCallAndSponsor-v1");
 
     function run() external {
         uint256 deployerPk = vm.envUint("DEPLOYER_KEY");
-        address deployer = vm.addr(deployerPk);
-        console.log("Deployer:", deployer);
+        // Get the bytecode of the contract
+        bytes memory bytecode = type(BatchCallAndSponsor).creationCode;
 
         // Start broadcasting transactions with Alice's private key.
         vm.startBroadcast(deployerPk);
 
-        // Deploy the delegation contract (Alice will delegate calls to this contract).
-        batchCaller = new BatchCallAndSponsor();
+        // Deploy using CREATE2
+        address payable deployedAddress = payable(Create2.deploy(0, SALT, bytecode));
+
+        // Cast the deployed address to BatchCallAndSponsor
+        batchCaller = BatchCallAndSponsor(deployedAddress);
 
         vm.stopBroadcast();
+        // Log the deployed address
+        console2.log("BatchCallAndSponsor deployed to:", deployedAddress);
     }
 }
